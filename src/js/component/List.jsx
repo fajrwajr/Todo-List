bd38ed2579a6712132d6f439ab4452330cb3a712
fajrwajr <bookreaderfajr@gmail.com>
2021-10-30 04:49:27 +0000

    Added counter to todo list

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 //include images into your bundle
 
 //create your first component
@@ -10,18 +10,29 @@ const List = () => {
 		setInputList(inputList.filter((result, i) => index != i));
 	};
 
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/santaMaria")
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then(data => setInputList(data));
+	}, []); //empty array means it will only run once
+
 	function addItem(e) {
 		if (e.keyCode == 13) {
 			setInputValue(inputValue);
 			const list = inputList.concat(inputValue);
 			setInputList(list);
-			console.log(inputList);
 		}
 	}
 
 	return (
 		<div className="text-center mt-5">
 			<h1>ToDo List</h1>
+			<h3>{inputList.length}</h3>
 			<input
 				type="text"
 				value={inputValue}
@@ -41,6 +52,7 @@ const List = () => {
 						</li>
 					</>
 				))}
+				<button onClick={() => setInputList([])}>Delete</button>
 			</div>
 		</div>
 	);
